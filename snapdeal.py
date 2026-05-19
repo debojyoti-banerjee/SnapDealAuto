6f3d8a5f128a954f511588fb57722c7d382075ea
debojyoti-banerjee <debojyotibanerjee2001@gmail.com>
2026-05-19 21:00:21 +0530

    first commit

---
+++ snapdeal.py
@@ -0,0 +1,47 @@
+import os
+from datetime import datetime, timezone
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.compute import ComputeManagementClient
+
+
+subscription_id = os.environ["SUBSCRIPTION_ID"]
+
+threshold_days = int(
+    os.environ["THRESHOLD_DAYS"]
+)
+
+
+credential = DefaultAzureCredential()
+
+
+compute_client = ComputeManagementClient(
+    credential,
+    subscription_id
+)
+
+
+current_time = datetime.now(timezone.utc)
+
+snapshots = compute_client.snapshots.list()
+
+for snapshot in snapshots:
+
+    snapshot_name = snapshot.name
+
+    resource_group = snapshot.id.split("/")[4]
+
+    creation_time = snapshot.time_created
+
+    age = (current_time - creation_time).days
+
+    if age > threshold_days:
+
+        print(f"Deleting {snapshot_name}")
+
+        delete_operation = compute_client.snapshots.begin_delete(
+            resource_group,
+            snapshot_name
+        )
+
+        delete_operation.wait()
+        print(f"{snapshot_name} deleted")
